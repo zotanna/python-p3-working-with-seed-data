16e6713ca24daafbcbe673dc31aed67290551f23
denis <denniiskim@gmail.com>
2024-02-12 15:06:45 +0300

    completed assignment

--- lib/seed.py
+++ lib/seed.py
@@ -14,3 +14,19 @@ if __name__ == '__main__':
     engine = create_engine('sqlite:///seed_db.db')
     Session = sessionmaker(bind=engine)
     session = Session()
+    session.query(Game).delete()
+    session.commit()
+
+    print("Seeding games...")
+
+    games = [
+        Game(
+            title=fake.name(),
+            genre=fake.word(),
+            platform=fake.word(),
+            price=random.randint(0, 60)
+        )
+        for i in range(50)]
+    
+    session.add_all(games)
+    session.commit()
